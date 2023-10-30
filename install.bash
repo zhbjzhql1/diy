98749efdb3d6307d2028e00419c86084d6dead29
ywt114 <58467878+ywt114@users.noreply.github.com>
2023-10-31 02:38:00 +0800

    Create install.bash

---
+++ install.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+URL="https://github.com/ywt114/kegin-package/releases/download/x86_64-luci-app-passwall_5.15/"
+DIRECTORY="/tmp/"
+
+# 使用wget下载指定目录下的所有.zip文件
+for file in $(wget -qO- $URL | grep -o '<a href="[^"]*\.ipk"' | sed 's/<a href="//' | sed 's/"//'); do
+    wget --no-check-certificate "$URL$file" -P "$DIRECTORY"
+done
