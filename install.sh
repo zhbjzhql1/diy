40d971080c1a658e44adb1e1ebf08b51d1d5f773
ywt114 <58467878+ywt114@users.noreply.github.com>
2023-10-31 12:57:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,3 @@
-#!/bin/sh
-
-url="https://github.com/ywt114/kegin-package/releases/download/x86_64-luci-app-passwall_5.15/"  # 指定要下载文件的基础URL
-directory="/small/"    # 指定要保存文件的本地目录
-
-# 使用 wget 下载目录内容并保存到文件
-wget --no-check-certificate -q -O - "$url" | grep -o -E 'href="[^"]+"' | cut -d'"' -f2 > files.txt
-
-# 逐行读取文件列表并下载每个文件
-while read -r file; do
-    wget --no-check-certificate "$url$file" -P "$directory"
-done < files.txt
-
-# 删除临时文件列表
-rm files.txt
+wget --no-check-certificate -O /tmp/passwall.tar.gz https://github.com/ywt114/kegin-package/releases/download/x86_64-luci-app-passwall_5.15/x86_64-luci-app-passwall_5.15.tar.gz && tar -xzvf passwall.tar.gz -C /tmp/ && opkg install /tmp/*.ipk --force-depends
+wget --no-check-certificate -O /tmp/ssr.tar.gz https://github.com/ywt114/kegin-package/releases/download/x86_64-luci-app-ssr-plus_5.15/x86_64-luci-app-ssr-plus_5.15.tar.gz && tar -xzvf ssr.tar.gz -C /tmp/ && opkg install /tmp/*.ipk --force-depends
+wget --no-check-certificate -O /tmp/openclash.tar.gz https://github.com/ywt114/kegin-package/releases/download/x86_64-luci-app-openclash_5.15/x86_64-luci-app-openclash_5.15.tar.gz && tar -xzvf ssr.tar.gz -C /tmp/ && opkg install /tmp/*.ipk --force-depends
