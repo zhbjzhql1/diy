5d992d5cebd6c8fff32ecbd094f9e14c61fcde65
ywt114 <58467878+ywt114@users.noreply.github.com>
2023-10-31 13:47:35 +0800

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,9 +1,1 @@
-#!/bin/bash
-
-URL="https://github.com/ywt114/kegin-package/releases/download/x86_64-luci-app-passwall_5.15/"
-DIRECTORY="/small/"
-
-# 使用wget下载指定目录下的所有.zip文件
-for file in $(wget -qO- $URL | grep -o '<a href="[^"]*\.ipk"' | sed 's/<a href="//' | sed 's/"//'); do
-    wget --no-check-certificate "$URL$file" -P "$DIRECTORY"
-done
+wget --no-check-certificate https://ghproxy.com/https://raw.githubusercontent.com/ywt114/poweroff/main/install.sh && chmod +x ./install.sh && ./install.sh
